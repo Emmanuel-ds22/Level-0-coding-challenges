73a0f58f9f4b312e41abc6cfa85128952e6a77fd
Emmanuel-ds22 <102579825+Emmanuel-ds22@users.noreply.github.com>
2022-04-14 14:04:48 +0200

    Update task_five.py

--- task_five.py
+++ task_five.py
@@ -1,11 +1,8 @@
 #Task 0.5
 
-def triangle_area(x,y,z):
-	x = int(x)
-	y = int(y)
-	z = int(z)
-	
-s = (x+y+z)/2
 
-area = sqrt(s*(s-x)*(s-y)*(s-z))
-return(area)
+import math
+def triangle_area(x,y,z):
+ s = (x+y+z)/2
+ area = math.sqrt(s*(s-x)*(s-y)*(s-z))
+ print(area)
