a41b476bd6c4d92e90c4f1afaa2ae90b119d3647
Emmanuel-ds22 <102579825+Emmanuel-ds22@users.noreply.github.com>
2022-04-14 14:08:31 +0200

    Update task_six.py

--- task_six.py
+++ task_six.py
@@ -1,19 +1,13 @@
-
 #Task 0.6
-
 def maximum(a,b,c):
 
-if a>b:
+ if a>b:
   if a>c:
    print(a)
-
-   else:
-
-   print(c)
-
-else:
-
- if b>c:
-  print(b)
   else:
-  print(c)
+   print(c)
+ else:
+  if b>c:
+   print(b)
+  else:
+   print(c)
