22069cd6888db9c95221aaf3c79e45fa15031ab2
Emmanuel-ds22 <102579825+Emmanuel-ds22@users.noreply.github.com>
2022-04-14 14:04:11 +0200

    Update task_eight.py

--- task_eight.py
+++ task_eight.py
@@ -1,5 +1,9 @@
-#Task 0.8
 
-def hours_and_minutes: 
-hours = num%60
-minutes = num//60
+
+#Task 0.8
+def hours_and_minutes(num): 
+ hours = num%60
+ minutes = num//60
+ print(str(hours)+"hours"+ str(minutes)+"minutes")
+ 
+hours_and_minutes(450)
