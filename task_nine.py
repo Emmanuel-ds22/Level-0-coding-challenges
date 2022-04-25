d2231b41f17dadf608171babd374d8d1da0bcf39
Emmanuel-ds22 <102579825+Emmanuel-ds22@users.noreply.github.com>
2022-04-25 15:29:07 +0200

    Update task_nine.py

--- task_nine.py
+++ task_nine.py
@@ -1,5 +1,11 @@
 def vowels(string):
- for i in range(0,len(string)):
-  if string[i] in "aeiou":
-   print(string[i],end=",")
-   
+ string2 =[]
+ 
+ for i in range(len(string)-1):
+  if string[i] not in string[i+1:]:
+   string2.append(string[i])
+ string2.append(string[len(string)-1]) 
+ 
+ for j in range(len(string2)):
+  if string2[j] in "aeiou":
+   print(string2[j],end=",")
