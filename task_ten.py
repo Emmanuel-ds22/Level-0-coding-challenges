3cc4c0d702ac30a4ce7c071338c901b33dbfe9ad
Emmanuel-ds22 <102579825+Emmanuel-ds22@users.noreply.github.com>
2022-04-26 14:32:31 +0200

    Update task_ten.py

--- task_ten.py
+++ task_ten.py
@@ -1,4 +1,11 @@
 def common_characters(letter1,letter2):
- for i in range(len(letter1)):
+ letter1 = letter1.lower()
+ letter2 = letter2.lower()
+ 
+ for i in range(len(letter1)-1):
   if letter1[i] in letter2:
    print(letter1[i],end=",")
+   
+ if letter1[len(letter1)-1] in letter2:  
+   print(letter1[len(letter1)-1],end="")
+   
