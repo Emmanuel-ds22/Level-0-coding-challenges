fb450940231cc571d138d874e197132fdc3c9cb8
Emmanuel-ds22 <102579825+Emmanuel-ds22@users.noreply.github.com>
2022-04-26 14:26:35 +0200

    Update task_nine.py

--- task_nine.py
+++ task_nine.py
@@ -1,11 +1,14 @@
 def vowels(string):
+ srting = string.lower()
  string2 =[]
- 
  for i in range(len(string)-1):
   if string[i] not in string[i+1:]:
    string2.append(string[i])
  string2.append(string[len(string)-1]) 
  
- for j in range(len(string2)):
+ for j in range(len(string2)-1):
   if string2[j] in "aeiou":
-   print(string2[j],end=",")
+   print(string2[j],end =",")
+   
+ if string2[len(string2)-1] in "aeiou":
+  print(string2[len(string2)-1])
