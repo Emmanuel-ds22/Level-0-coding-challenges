b568cd4874b51a530b2bf09e045b1bb1a67cb193
Emmanuel-ds22 <102579825+Emmanuel-ds22@users.noreply.github.com>
2022-04-26 15:50:11 +0200

    Update task_five.py

--- task_five.py
+++ task_five.py
@@ -1,5 +1,5 @@
-import math
 def triangle_area(side_x,side_y,side_z):
  semiperimeter = (side_x+side_y+side_z)/2
- area = math.sqrt(semiperimeter*(semiperimeter-side_x)*(semiperimeter-side_y)*(semiperimeter-side_z))
+ area_squared = semiperimeter*(semiperimeter-side_x)*(semiperimeter-side_y)*(semiperimeter-side_z)
+ area = area_squared**0.5
  return area
