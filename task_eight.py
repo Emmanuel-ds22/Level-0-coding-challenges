6a2b881b8f14067279316b903d9246b297fd2d4f
Emmanuel-ds22 <102579825+Emmanuel-ds22@users.noreply.github.com>
2022-05-03 15:19:08 +0200

    Update task_eight.py

--- task_eight.py
+++ task_eight.py
@@ -1,13 +1,13 @@
-def hours_and_minutes(num): 
- minutes = num%60
- hours = num//60
- if minutes ==1:
-  if hours == 1:
-   print(f"{hours} hour and {minutes}minute")
-  else:
-   print(f"{hours} hours and {minutes}minute")
- else:
-  if  hours == 1:
-   print(f"{hours} hour and {minutes}minutes")
-  else:
-   print(f"{hours} hours and {minutes}minutes") 
+def hours_and_minutes(num):
+    minutes = num % 60
+    hours = num // 60
+    if minutes == 1:
+        if hours == 1:
+            print(f"{hours} hour and {minutes}minute")
+        else:
+            print(f"{hours} hours and {minutes}minute")
+    else:
+        if hours == 1:
+            print(f"{hours} hour and {minutes}minutes")
+        else:
+            print(f"{hours} hours and {minutes}minutes")
