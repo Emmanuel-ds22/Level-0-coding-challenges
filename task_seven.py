48aaeb9dbcf3379fdaea16c07488125d10cfdb4d
Emmanuel-ds22 <102579825+Emmanuel-ds22@users.noreply.github.com>
2022-05-03 15:15:08 +0200

    Update task_seven.py

--- task_seven.py
+++ task_seven.py
@@ -1,7 +1,8 @@
 def celsius_to_fahrenheit(num):
- fahrenheit = num*(9/5) + 32
- return fahrenheit
+    fahrenheit = num * (9 / 5) + 32
+    return fahrenheit
+
 
 def fahrenheit_to_celsius(num):
-  celsius = (num - 32)*(5/9)
-  return celsius
+    celsius = (num - 32) * (5 / 9)
+    return celsius
