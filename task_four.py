acab7d0eedff51a026602a3005222bb43a7f0422
Emmanuel-ds22 <102579825+Emmanuel-ds22@users.noreply.github.com>
2022-05-03 15:17:53 +0200

    Update task_four.py

--- task_four.py
+++ task_four.py
@@ -1,7 +1,8 @@
 def even_or_odd(num_x):
- if num_x%2 == 0:
-  print("even")
- else:
-  print("odd")
-  
-even_or_odd(13)  
+    if num_x % 2 == 0:
+        print("even")
+    else:
+        print("odd")
+
+
+even_or_odd(13)
