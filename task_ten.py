83b7e50cbda1fdd29ae7593ed43cebd483864e42
Emmanuel-ds22 <102579825+Emmanuel-ds22@users.noreply.github.com>
2022-05-03 14:07:02 +0200

    Update task_ten.py

--- task_ten.py
+++ task_ten.py
@@ -2,14 +2,19 @@ def common_characters(letter1,letter2):
  letter1 = letter1.lower()
  letter2 = letter2.lower()
  common_letters = []
+ common_letters2 = []
  
  for i in range(len(letter1)-1):
   if letter1[i] in letter2:
    common_letters.append(letter1[i])
+  
+ for i in range(len(common_letters)-1):
+   if common_letters[i] not in common_letters[i+1:]:
+    common_letters2.append(common_letters[i])
+ common_letters2.append(common_letters[len(common_letters)-1]) 
    
  print("Common letters: ", end="")
- for j in range(len(common_letters)-1):
-   print(common_letters[j],end =",")
- if len(common_letters)>0:
-   print(common_letters[len(common_letters)-1])
-  
+ for j in range(len(common_letters2)-1):
+   print(common_letters2[j],end =",")
+ if len(common_letters2)>0:
+   print(common_letters2[len(common_letters2)-1])
