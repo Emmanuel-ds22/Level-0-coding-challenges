0f5c8027dd87f7622b8fd6f9dd0ec60cb42619d3
Emmanuel-ds22 <102579825+Emmanuel-ds22@users.noreply.github.com>
2022-05-03 15:14:14 +0200

    Update task_six.py

--- task_six.py
+++ task_six.py
@@ -1,12 +1,11 @@
-def maximum(num_a,num_b,num_c):
- if num_a>num_b:
-  if num_a>num_c:
-   return num_a
-  else:
-   return num_c
- else:
-  if num_b>num_c:
-   return num_b
-  else:
-   return num_c
-  
+def maximum(num_a, num_b, num_c):
+    if num_a > num_b:
+        if num_a > num_c:
+            return num_a
+        else:
+            return num_c
+    else:
+        if num_b > num_c:
+            return num_b
+        else:
+            return num_c
